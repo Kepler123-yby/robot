c9b93e5342916841012aae66883fd6e8666d9a59
Kepler123-yby <3827200344@qq.com>
2025-08-14 21:51:26 +0800

    Version2

--- workdown/workdown_ws/image_topic/src/publisher.cpp
+++ workdown/workdown_ws/image_topic/src/publisher.cpp
@@ -1,17 +1,20 @@
 #include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/image.hpp"
 
 class ImageTopicPublisher : public rclcpp::Node{
 
 public:
-    
+    ImageTopicPublisher(std::string name) : Node(name){
+        RCLCPP_INFO(this->get_logger(),"节点已经启动:%s",name.c_str());
+    }
 private:
 
 };
 
 int main(int argc, char **argv){
     rclcpp::init(argc,argv);
-    auto node = std::make_shared<ImageTopicPublisher>("Image_Topiic_Publisher");
+    auto node = std::make_shared<ImageTopicPublisher>("Image_Topic_Publisher");
     rclcpp::spin(node);
-    rclcpp::shutdown;
+    rclcpp::shutdown();
     return 0;
 }
