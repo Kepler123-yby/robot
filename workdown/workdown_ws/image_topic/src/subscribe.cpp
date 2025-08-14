210265618b556ff7bcef805e5f535126f18c39d9
Kepler123-yby <3827200344@qq.com>
2025-08-15 00:59:29 +0800

    subscribe03

--- workdown/workdown_ws/image_topic/src/subscribe.cpp
+++ workdown/workdown_ws/image_topic/src/subscribe.cpp
@@ -1,14 +1,19 @@
 #include "rclcpp/rclcpp.hpp"
+#include "aim_interfaces/msg/aim_info.hpp"
 
 class ImageTopicSubscribe : public rclcpp::Node{
 
 public:
     ImageTopicSubscribe(std::string name) : Node(name){
         RCLCPP_INFO(this->get_logger(),"节点已经启动:%s",name.c_str());
+        aim_target =this->create_subscription<aim_interfaces::msg::AimInfo>("aim",10,std::bind(&ImageTopicSubscribe::aim_callback,this,std::placeholders::_1));
     }
     
 private:
+    rclcpp::Subscription<aim_interfaces::msg::AimInfo>::SharedPtr aim_target;
+    void aim_callback(){
 
+    }
 };
 
 int main(int argc, char **argv){
