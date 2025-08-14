70fd3e81526e861df0745e2e56612e587c0250e2
Kepler123-yby <3827200344@qq.com>
2025-08-15 00:58:54 +0800

    publisher04

--- workdown/workdown_ws/image_topic/src/publisher.cpp
+++ workdown/workdown_ws/image_topic/src/publisher.cpp
@@ -6,11 +6,15 @@ class ImageTopicPublisher : public rclcpp::Node{
 public:
     ImageTopicPublisher(std::string name) : Node(name){
         RCLCPP_INFO(this->get_logger(),"节点已经启动:%s",name.c_str());
-        sensor_img = this->create_publisher<sensor_msgs::msg::Image>("command",10);
+        sensor_img = this->create_publisher<sensor_msgs::msg::Image>("sensor",10);
+        time = this->create_wall_timer(std::chrono::milliseconds(500),std::bind(&ImageTopicPublisher::timer_callback,this));
     }
 private:
     rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr sensor_img;
+    rclcpp::TimerBase::SharedPtr time;
+    void timer_callback(){
 
+    }
 };
 
 int main(int argc, char **argv){
