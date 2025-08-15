ca8decbdfce6f52059206cdf0e34f52e67c01082
Kepler123-yby <3827200344@qq.com>
2025-08-15 23:02:59 +0800

    subscribe03

--- workdown/workdown_ws/image_topic/src/subscribe.cpp
+++ workdown/workdown_ws/image_topic/src/subscribe.cpp
@@ -1,5 +1,8 @@
 #include "rclcpp/rclcpp.hpp"
 #include "aim_interfaces/msg/aim_info.hpp"
+#include <opencv2/opencv.hpp>
+#include <cv_bridge/cv_bridge.h>
+#include "sstream"
 
 class ImageTopicSubscribe : public rclcpp::Node{
 
@@ -11,8 +14,14 @@ public:
     
 private:
     rclcpp::Subscription<aim_interfaces::msg::AimInfo>::SharedPtr aim_target;
-    void aim_callback(){
-
+    void aim_callback(const aim_interfaces::msg::AimInfo::SharedPtr msg){
+        std::ostringstream oss;
+        oss << "装甲板坐标：[";
+        for(size_t i = 0; i < msg->coordinate.size(); ++i){
+            oss << msg->coordinate[i] << (i + 1 == msg->coordinate.size() ? "" : ", ");
+        }
+        oss << "],装甲板图案:"<< msg->type;
+        RCLCPP_INFO(this->get_logger(),"%s",oss.str().c_str());
     }
 };
 
